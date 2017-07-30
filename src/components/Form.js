bad71268c3f0e5324af5687cbf3fb601cdbb35c4
JordanPak <jpak@jordanpak.com>
2017-07-30 23:06:54 +0000

    Add Form component

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Form extends Component {
+
+	render() {
+		return (
+			<h1>Look at this chicken</h1>
+		);
+	}
+}
+
+export default Form;
--- src/index.js
+++ src/index.js
@@ -5,23 +5,36 @@ import ReactDOM from 'react-dom';
 // service worker
 import registerServiceWorker from './registerServiceWorker';
 
+// components
+import Form from './components/Form';
+
 // assets
-import logo from './images/logo.svg';
+// import logo from './images/logo.svg';
 import './css/App.css';
 
 
 const App = () => (
-	<div className="App">
+	<main className="App">
 
-		<div className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<h2>Welcome to React</h2>
-		</div>
-		
-		<p className="App-intro">
-			To get started, edit <code>src/App.js</code> and save to reload.
-		</p>
-	</div>
+		{/* FORM */ }
+		<Form />
+
+		{/* TAG GRID */ }
+
+	</main>
+
+
+	// <div className="App">
+
+	// 	<div className="App-header">
+	// 		<img src={logo} className="App-logo" alt="logo" />
+	// 		<h2>Welcome to React</h2>
+	// 	</div>
+
+	// 	<p className="App-intro">
+	// 		To get started, edit <code>src/App.js</code> and save to reload.
+	// 	</p>
+	// </div>
 );
 
 
