a893f766c33cbe1327362e07c3204b41f3c0a350
JordanPak <jpak@jordanpak.com>
2017-07-30 22:48:47 +0000

    Remove the app.js stuff - merge into index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
+// libs
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+// service worker
 import registerServiceWorker from './registerServiceWorker';
 
+// assets
+import logo from './images/logo.svg';
+import './css/App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+
+// start the death!
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
