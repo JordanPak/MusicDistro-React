aca5b658892f8f9f16c32e6c008a72f70aa0fc2c
JordanPak <jpak@jordanpak.com>
2017-07-30 23:59:54 +0000

    Do some form styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,22 @@ class Form extends Component {
 
 	render() {
 		return (
-			<h1><i className="fa fa-file-text"></i> Uhuh</h1>
+			<section className="form">
+				<form action="">
+
+					<label htmlFor="instrument">Select An Instrument</label>
+
+					<select name="instrument" id="instrument">
+						<option value="piccolo">Piccolo</option>
+						<option value="clarinet">Clarinet</option>
+						<option value="alto-sax">Alto Sax</option>
+						<option value="tenor-sax">Tenor Sax</option>
+					</select>
+
+					<input type="submit" value="Go" />
+
+				</form>
+			</section>
 		);
 	}
 }
