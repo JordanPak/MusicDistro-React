31c9ae11e8b0ef4d2aaeb4dd194c7d3f981b93e3
JordanPak <jpak@jordanpak.com>
2017-07-30 22:53:10 +0000

    Put app in a constant

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // libs
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 // service worker
@@ -10,21 +10,19 @@ import logo from './images/logo.svg';
 import './css/App.css';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+	<div className="App">
+
+		<div className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<h2>Welcome to React</h2>
+		</div>
+		
+		<p className="App-intro">
+			To get started, edit <code>src/App.js</code> and save to reload.
+		</p>
+	</div>
+);
 
 
 // start the death!
