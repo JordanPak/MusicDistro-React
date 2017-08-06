2ea95a8346f85b41a2dd65f0cb14fa123db04852
JordanPak <jpak@jordanpak.com>
2017-08-06 22:20:44 +0000

    Clean up sheetData

--- src/sheetData.js
+++ src/sheetData.js
@@ -8,10 +8,11 @@
 // get sheet music
 import sheetMusic from './sheet-music.json';
 
+
 /**
  * Tag / instrument store
  */
-var allTags        = []; //{};
+var allTags        = [];
 var allInstruments = [];
 // var allSongs       = [];
 
@@ -36,40 +37,30 @@ function setTag( tag ) {
 }
 
 
-
+/**
+ * Arrangement loop
+ */
 for ( var [ song, data ] of Object.entries( sheetMusic ) ) {
 
 	// add the song
 	// allSongs = allSongs.concat( song );
 
-
-	// loop through tags and add the
-	// current song to the list
+	// set arrangement tags
 	data.tags.forEach( setTag );
-
 
 	// remove tag from the object so we just
 	// have instruments
 	delete data.tags;
-	
-	console.log( data );
 
-	// traverse instruments
-	for ( var [ instrument, links ] of Object.entries( data ) ) {
-		// console.log( instrument, links );
+	// traverse and add instruments
+	for ( var [ instrument ] of Object.entries( data ) ) {
 		allInstruments = allInstruments.concat( instrument );
 	}
-	
-	console.log( '-------------------------------------------' );
 }
 
-// filter duplicates
-console.log( 'ALL TAGS', allTags );
+
+// filter duplicate instruments
 allInstruments = Array.from( new Set( allInstruments ) );
-
-
-// testing
-console.log( '====================================================' );
 
 
 // configure main object
