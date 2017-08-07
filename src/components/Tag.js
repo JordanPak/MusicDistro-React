09ea25a59451de818fac9eaf7136c1f1e98543d5
JordanPak <jpak@jordanpak.com>
2017-08-07 03:09:47 +0000

    Clean up Tag rendering

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -19,15 +19,14 @@ class Tag extends Component {
 	render() {
 
 		// quit if no arrangements
-		console.log( this.props.arrangements );
-
 		if ( Object.keys( this.props.arrangements ).length === 0 ) {
-			console.log( this.props.title + ' has no arrangements for the selected instrument' );
 			return null;
 		}
 
 		return (
-			<figure><figcaption>{ this.props.title }</figcaption></figure>
+			<figure>
+				<figcaption>{ this.props.title }</figcaption>	
+			</figure>
 		);
 	}
 }
